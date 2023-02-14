3b12dd048dc601dc9dd880000086ebf0f968dc62
Jakubpatelski <jakubpatelski24@gmail.com>
2023-02-14 11:15:24 +0100

    Initial Commit

---
+++ NotesAppHomework/NotesAppHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppHomeworkApp.swift
+//  NotesAppHomework
+//
+//  Created by Jakub Patelski on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
